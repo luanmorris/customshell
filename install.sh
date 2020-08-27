8e67c2ea1f27a3dddf43982871caf6e8d407f5ce
Luan Pereira <luanpsmorris@gmail.com>
2020-08-26 23:45:54 -0300

    Creation of conditional for installarion.

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-cat parameters >> fileTest
+install() {
+	local cat_bashrc="$(cat ~/.bashrc 2> /dev/null)"
+
+	if [[ $cat_bashrc =~ "CUSTOM SHELL PARAMETERS" ]]; then 
+		echo "Custom Shell has been installed."
+	else
+		cp ~/.bashrc ~/.bashrc_copy && cat parameters >> ~/.bashrc && echo "Instalation successful"
+	fi
+}
+
+findbrc="$(ls ~/.bashrc 2> /dev/null)"
+
+	if [[ $findbrc =~ "bashrc" ]]; then
+		echo $(install)
+	else
+		echo "Your system does not have the “bashrc” file."
+	fi
